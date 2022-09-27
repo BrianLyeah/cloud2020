19cb4995555c9bf5374b1d68bbc52f502265771d
L <brianl@yeah.net>
2022-09-27 23:32:38 +0800

    Msg:1、新增spring-cloud-bus消息总线；2、使用spring-config配置中心后，各个client从server同步，但是需要重启服务；3、spring-cloud-bus消息总线是轻量级的消息代理，通常会使用消息代理rabbitmq、kafka来构建一个主题，然后把微服务架构中的所有服务都连接到这个主题上去，当我们向该主题发送消息时，所有订阅该主题的服务都会收到消息并进行消费；4、所以给config-server发送刷新消息后，各个client都会收到消息进行消费，进行配置文件的刷新；也可以发送refresh刷新消息的时候，指定"服务名:端口"对指定服务发送消息进行刷新；

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RefreshScope
+public class ConfigController {
+
+    @Value("${info.uri}")
+    private String infoUri;
+
+    @GetMapping(value = "/config/getUri")
+    public String getUri(){
+        return  infoUri;
+    }
+
+}
