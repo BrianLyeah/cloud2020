1dc41087524f99e4f344da62984b6a9549abf8aa
L <brianl@yeah.net>
2022-10-13 23:25:48 +0800

    Msg:1、sentinel的热点限流；2、@SentinelResource中配置热点限流的key，以及兜底的blockHandler兜底方法。该兜底方法只会处理sentinel控制台配置的限流规则情况，处理的是alibaba的BlockException，并不会处理代码中出现的runtimeException异常；3、热点限流指定了需要限流的热点key以及参数索引位置，该索引位置指的是代码方法中的参数index，而不是url中的参数位置；4、热点key限流还能在通用限流规则中，对特例的参数进行更深一层的限流，如：param1的限流规则的qps是1，当param1=123时，该特定值的时候qps=200；

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,12 +1,19 @@
 package com.atguigu.springcloud.alibaba.controller;
 
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.sun.deploy.security.BlockedException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
+@Slf4j
 public class FlowLimitController
 {
     @GetMapping("/testA")
@@ -31,6 +38,17 @@ public class FlowLimitController
         return "------testD  "+ UUID.randomUUID();
     }
 
+    @GetMapping("/testHostKey")
+    @SentinelResource(value = "testHostKey",blockHandler = "hostKeyBlockHandler")
+    public String testHostKey(@RequestParam(value = "p1",required = false)String p1,@RequestParam(value = "p2",required = false)String p2) {
+        return "------hostKey  "+ UUID.randomUUID();
+    }
+
+    public String hostKeyBlockHandler(String p1, String p2, BlockException exp)
+    {
+        exp.printStackTrace();
+        return p1+"----"+p2+"------hostKeyBlockHandler 哭哭哭";
+    }
 
 
 }
